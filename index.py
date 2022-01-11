32c021086cef433d89652100db7e7d85aba1428b
Simone Mattiolo <simone.mattiolo@rdcom.com>
2022-01-11 09:47:02 +0100

    a

--- index.py
+++ index.py
@@ -1,1 +1,14 @@
-print("alpha")
+print('Opening test.txt')
+
+with open('./test.txt', 'a') as test_file:
+  userInput = input('Write something into test.txt: ') + "\n"
+  print('Writing into test.txt...')
+  test_file.write(userInput)
+
+print('\n\nReading test.txt...')
+
+with open('./test.txt', 'r') as test_file:
+  content = test_file.read()
+  print(content)
+
+print('END')
